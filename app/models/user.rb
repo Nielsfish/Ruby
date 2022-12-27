ee4bb8adcd8b526d801f5e2b2803cd5f87091816
Nielsfish <259687@student.pwr.edu.pl>
2022-12-27 13:47:12 +0000

    z haslem digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+    validates :login, presence: true, uniqueness: true, length: { in: 3..50 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :account_type, presence: true
+    validates :account_no, presence: true, uniqueness: true, length: { is: 28 }
+    has_secure_password
+    
     has_many :employees
     has_many :gates
     has_many :managers
--- db/migrate/20221218215819_add_password_to_user.rb
+++ db/migrate/20221218215819_add_password_to_user.rb
@@ -1,5 +1,5 @@
-class AddPasswordToUser < ActiveRecord::Migration[7.0]
+class AddPasswordDigestToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :password, :string
+    add_column :users, :password_digest, :string
   end
 end
